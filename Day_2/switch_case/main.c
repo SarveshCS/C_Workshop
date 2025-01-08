1ae92c5a142b8924909a3141c1465f199101bd5d
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-08 09:44:58 +0530

    Correction: Added a practice problem folder.

--- Day_2/switch_case/main.c
+++ Day_2/switch_case/main.c
@@ -35,19 +35,3 @@
 //     }
 
 // }
-
-// WAP to convert an upper case letter to a lower case letter
-
-#include<stdio.h>
-void main(){
-    char a;
-    // printf("%d %d", 'A', 'Z');
-    printf("Enter the char: ");
-    scanf("%c", &a);
-    if (a>=97 && a<=122){
-        printf("%c", a-32);
-    }
-    if (a>=65 && a<=90){
-        printf("%c", a+32);
-    }
-}
