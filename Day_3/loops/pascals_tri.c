0baa0a8509e0de331487566153574d9e6f3fbbec
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-09 09:56:05 +0530

    Day 4: updated the pascals triangle using binomial relation in the nCr formulas

--- Day_3/loops/pascals_tri.c
+++ Day_3/loops/pascals_tri.c
@@ -1,5 +1,18 @@
 // Pttern practice
 #include<stdio.h>
 void main(){
-    // Will be implimented later
+    int n, i, j, val;
+    printf("Enter: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++){
+        val = 1;
+        for (j = 0; j < n-i-1; j++){
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++){
+            printf("%d ", val);
+            val = val * (i - j)/(j+1);
+        }
+        printf("\n");
+    }
 }
