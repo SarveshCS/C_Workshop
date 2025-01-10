b4fd6d03232c1eec5ddf644eb99c04d687da5a1c
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-10 11:36:55 +0530

    Chnanged the fibonacci function a bit

--- Day_4/fibonacci/recursion.c
+++ Day_4/fibonacci/recursion.c
@@ -2,13 +2,8 @@
 #include<stdio.h>
 
 int fibo(int n){
-    if (n == 1){
-        return 0;
-    }
-    if (n == 2){
-        return 1;
-    }
-
+    if (n == 1){ return 0; }
+    if (n == 2){ return 1; }
     return fibo(n-1) + fibo(n-2);
 }
 
