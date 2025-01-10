740c0e17d9f6707deeeeb43bbb519cccc4d5bb4a
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-10 19:30:50 +0530

    Solved the rage based recursive operations question in a single recursion function

--- Day_5/diff_op/main_better.c
+++ Day_5/diff_op/main_better.c
@@ -1,60 +1,36 @@
 #include<stdio.h>
 
-float rec(int n, int l, int u){
-    if (!u && !l){
-        if (n <= 10 && n>=0 ){
-            l = 1; u = 10;
-            printf("%d", ad(l, u));
-        }
-        else if (n <= 20 && n>=11 ){
-            l = -20; u = -11;
-            printf("%d", su(l, u));
-        }
-        else if (n <= 30 && n>=21 ){
-            l = 21; u = 30;
-            printf("%f", mu(l, u));
-        }
-        else if (n <= 40 && n>=31 ){
-            l = 31; u = 40;
-            printf("%Le", di(l, u));
-        }
-    }else{
-        
+double rec(double l, double n, int op){
+    if (n <= l) { return l; }
+    if (!op) {
+        return n + rec(l, n-1, op);
+    } else if (op == 1) {
+        return rec(l, n-1, op) - n;
+    } else if (op == 2) {
+        return n * rec(l, n-1, op);
+    } else if (op == 3) {
+        return rec(l, n-1, op) / n;
     }
-    
-}
-int ad(int l, int n){
-    if (n <= l){
-        return l;
-    }
-    return n + ad(l, n - 1);
-}
-
-int su(int l, int n){
-    if (n <= l){
-        return l;
-    }
-    return n + (su(l, n - 1));
-}
-
-float mu(float l, float n){
-    if (n <= l){
-        return l;
-    }
-    return n * mu(l, n - 1);
-}
-
-double di(double l, double n){
-    if (n <= l){
-        return l;
-    }
-    return di(l, n - 1) / n;
 }
 
 void main(){
-    int n;
-    double l, u;
+    int n, op = 0;
+    double l = 1, u = 10;
     printf("Enter the number: ");
     scanf("%d", &n);
-    rec(n, 0, 0);
+    if (n <= 10 && n>=1 ){
+        printf("Addition: %.0lf", rec(l, u, op));
+    }
+    else if (n <= 20 && n>=11 ){
+        op  = 1;
+        printf("Subtraction: %.0lf", rec(l+=10, u+=10, op));
+    }
+    else if (n <= 30 && n>=21 ){
+        l = 21; u = 30; op = 2;
+        printf("Multiplication: %.0lf", rec(l+=20, u+=20, op));
+    }
+    else if (n <= 40 && n>=31 ){
+        l = 31; u = 40; op = 3;
+        printf("Division: %le", rec(l+=30, u+=30, op));
+    }
 }
