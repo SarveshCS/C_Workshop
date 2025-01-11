9269e8027d65e7a071a6cb1e51b5c9693c12fd18
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-11 11:23:24 +0530

    Simple 2D array and array using pointer address

---
+++ Day_6/array/2d_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int n, m, i, j;
+    printf("Enter the size of diamension 1: ");
+    scanf("%d", &n);
+    printf("Enter the size of diamension 2: ");
+    scanf("%d", &m);
+
+    int arr[n][m];
+    for (i = 0; i < n; i++){
+        for (j = 0; j < m; j++){
+            printf("Enter the value at index (%d, %d): ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // for (i = 0; i < n; i++){
+    //     for (j = 0; j < n; j++){
+    //         printf("Value arr[%d][%d] = %d \n", i, j , *(arr+i));
+    //     }
+    // }
+
+    // Matrix-like format
+    for (i = 0; i < n; i++){
+        printf("[");
+        for (j = 0; j < m; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\b]\n");
+    }
+}
--- Day_6/array/creating_array.c
+++ Day_6/array/creating_array.c
@@ -7,7 +7,7 @@ void main(){
 
     int arr[n];
     for (i = 0; i < n; i++){
-        printf("Enter the value %d: ", i+1);
+        printf("Enter the value at index %d: ", i);
         scanf("%d", arr+i);
     }
 
