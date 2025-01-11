a4f086042e4c3b34c44bbae4195bd8059c5c0105
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-11 13:35:21 +0530

    Updated the .gitignore

--- Day_6/array/tmp_question.c
+++
@@ -1,1 +0,0 @@
-// Given an int arry num[] sorted in assending order, remove the duplicates in place such that each unique element apears only once the relative order of the elements should be kept the same
