c0bc07d0bedd73f439d15e0d22a51600124efd84
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-01-16 10:35:45 +0530

    Dynamic memory allocation malloc() function

---
+++ Day_10/dynamic_mem/basic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int i;
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++){
+        printf("Enter the value %d: ", i+1);
+        scanf("%d", arr+i);
+    }
+
+    for (i = 0; i < n; i++){
+        printf("Value arr[%d] = %d\n", i , *(arr+i));
+    }
+
+    free(arr);
+}
